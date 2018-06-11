2845cd2472062187373c428b31daf21c4bda034d
Oconrad <oconrad93@gmail.com>
2018-06-11 18:22:10 +0200

    attempt module5

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,18 +9,20 @@ class Quiz extends Component {
     super(props)
     this.state = {quiz_position: 1}
   }
+  showNextQuestion() {
+    this.setState((state) => {
+      return { quiz_position: state.quiz_position + 1}
+    })
+  }
   render() {
        let finalComponent = null
-        const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length ? true : false;
-        if (isQuizEnd)
-            finalComponent = <QuizEnd />
-        else
-            finalComponent = <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
-
-         return ( <div>
-                    {finalComponent}
-                 </div>
-         );
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
+        return(
+          <div>
+            {isQuizEnd ? <QuizEnd/>:
+            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/>}
+          </div>
+        )
      }
 }
 
